13e7f86f4e4ac542b2cdc8ce98fc9c3ed217e82a
KorKux <cristhianeduardo03@hotmail.com>
2020-09-28 17:22:27 -0500

    feat: init project

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,2 +1,12 @@
-package main;public class Main {
+package main;
+
+/**
+ * Main class for Sleepy Monitor
+ *
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
 }
--- src/model/Student.java
+++ src/model/Student.java
@@ -1,2 +1,4 @@
-package model;public class Student {
+package model;
+
+public class Student {
 }
